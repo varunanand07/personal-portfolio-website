3fa1bd211f865c3566ee2a053376305c34206641
varunanand07 <anandvarun2003@gmail.com>
2024-10-02 22:44:07 +0100

    Added a Subheading under my name and styled it

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,10 @@ const Hero = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1>Varun Anand</h1>
+            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Varun Anand</h1>
+            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"> 
+            Full Stack Developer
+            </span>
           </div>
         </div>
     </div>
