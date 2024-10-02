9466b589ad8f26a567149510defe23f179f2beb9
varunanand07 <anandvarun2003@gmail.com>
2024-10-02 22:17:41 +0100

    Added a Radial Background to the Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/NavBar"
 const App = () => {
   return (
     <div>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       <Navbar />
     </div>
   )
