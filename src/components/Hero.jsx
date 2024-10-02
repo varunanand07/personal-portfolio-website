92662d9af163b284f311e2adf43c723cce6b6218
varunanand07 <anandvarun2003@gmail.com>
2024-10-03 00:50:04 +0100

    Styled the about me section and added the profile picture next to it

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,5 @@
+import { HERO_CONTENT } from "../constants"
+import profilePic from "../assets/kevinRushProfile.png";
 
 const Hero = () => {
   return (
@@ -9,7 +11,13 @@ const Hero = () => {
             <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"> 
             Full Stack Developer
             </span>
+            <p className="my-2 max-w-xl py-6 font-light tracking-tighter">{HERO_CONTENT}</p>
           </div>
+        </div>
+        <div className="w-full lg:w-1/2 lg:p-8">
+            <div className="flex justify center">
+                <img src={profilePic} alt="Varun Anand" />
+            </div>
         </div>
     </div>
 </div>
