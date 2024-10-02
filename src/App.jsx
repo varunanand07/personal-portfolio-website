a5eac00b6c1afc1854ae015aca02bfb9c77a3cbe
varunanand07 <anandvarun2003@gmail.com>
2024-10-02 22:03:53 +0100

    Fixed an issue where my Navbar wasn't visible. The issue was in the App.jsx file, the Navbar component was not imported correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
+import Navbar from "./components/NavBar"
 
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <Navbar />
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
+import logo from "../assets/kevinRushLogo.png";
 
 const NavBar = () => {
-  return (
-    <div>
-      
+  return <nav className="bg-red-900 mb-20 flex items-center justify-between py-6">
+    <div className="flex flex-shrink-0 items-center">
+        <img src={logo} alt="logo" />    
     </div>
-  )
+  </nav>
 }
 
 export default NavBar
