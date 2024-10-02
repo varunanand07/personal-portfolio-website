271ba18587aa80dcc8bd7f84faa35cf014c539b1
varunanand07 <anandvarun2003@gmail.com>
2024-10-02 22:34:48 +0100

    Added a new component file, Hero.jsx. This will be part of the middle section of the page where my Name and About section will be present

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+
+const Hero = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4 lg:mb-36">
+      <div className="flex flex-wrap">
+        <div className="w-full lg:w-1/2">
+          <div className="flex flex-col items-center lg:items-start">
+            <h1>Varun Anand</h1>
+          </div>
+        </div>
+    </div>
+</div>
+  )
+}
+
+export default Hero
