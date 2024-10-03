0e13112dbbf7b9ed5b7c48bae1fd49ddcfa99e7e
varunanand07 <anandvarun2003@gmail.com>
2024-10-03 12:59:56 +0100

    Experiences section completed, added languages and frameworks used and styled them

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -19,6 +19,10 @@ const Experience = () => {
                           {Experience.company}
                         </span>
                       </h6>
+                      <p className='mb-4 text-neutral-400'>{Experience.description}</p>
+                      {Experience.technologies.map((tech, index) => (
+                        <span key={index} className='mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>{tech}</span>
+                      ))}
                     </div>
                 </div>
                ))}
