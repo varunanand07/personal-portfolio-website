441e1190e00e06e5068ba85e3e5c9bce7cd99a62
varunanand07 <anandvarun2003@gmail.com>
2024-10-03 13:30:52 +0100

    Adjusted image sizing

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,8 +4,10 @@ const Projects = () => {
   return (<div className="border-b border-neutral-900 pb-4">
     <h2 className="my-20 text-center text-4xl">Projects</h2>
     <div>{PROJECTS.map((project, index) =>  (
-        <div key= {index}>
-            <img src={project.image} alt={project.title} />
+        <div key= {index} className="mb-8 flex flex-wrap lg:justify-center">
+            <div className="w-full lg:w-1/4">
+            <img src={project.image} width={150} height={150} alt={project.title} />
+            </div>
         </div>
     ))}
     </div>
