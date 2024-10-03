1562541cff406bf7a13a12bad660ea3300a9981e
varunanand07 <anandvarun2003@gmail.com>
2024-10-03 12:51:37 +0100

    Added role details and applied style, white text for the role and light purple text for the company

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -10,7 +10,15 @@ const Experience = () => {
                 <div key={index} className='mb-8 flex flex-wrap lg:justify-center'
                 >
                     <div className="w-full lg:w-1/4">
-                        <p>{Experience.year}</p>
+                        <p className='mb-2 text-sm text-neutral-400'>{Experience.year}</p>
+                    </div>
+                    <div className="w-full max-w-xl lg:w-3/4">
+                      <h6 className='mb-2 font-semibold'>
+                        {Experience.role} - {" "}
+                        <span className='text-sm text-purple-200'>
+                          {Experience.company}
+                        </span>
+                      </h6>
                     </div>
                 </div>
                ))}
