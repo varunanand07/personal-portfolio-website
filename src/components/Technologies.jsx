950effb3530e3375c03385a07377d459b3cfc843
varunanand07 <anandvarun2003@gmail.com>
2024-10-05 20:13:54 +0100

    Applied Framer Motion animation to the Tech Stack section

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -8,46 +8,113 @@ import { BiLogoPostgresql } from "react-icons/bi";
 import { FaHtml5 } from "react-icons/fa";
 import { IoLogoCss3 } from "react-icons/io";
 import { FaDocker } from "react-icons/fa";
+import { motion } from "framer-motion";
 
-
-
-
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
 
 const Technologies = () => {
   return (
-  <div className="border-b border-neutral-800 pb-24">
-    <h2 className="my-20 text-center text-4xl">Tech Stack</h2>
-    <div className="flex flex-wrap items-center justify-center gap-4">
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <FaHtml5 className="text-7xl text-orange-400" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <IoLogoCss3 className="text-7xl text-blue-500" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <IoLogoJavascript className="text-7xl text-yellow-300" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <RiReactjsLine className="text-7xl text-cyan-400" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <FaNodeJs className="text-7xl text-green-500" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <BiLogoPostgresql className="text-7xl text-sky-700" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <FaAws className="text-7xl text-white-700" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <FaPython className="text-7xl text-yellow-300" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
-            <FaDocker className="text-7xl text-blue-700" />
-        </div>
+    <div className="border-b border-neutral-800 pb-24">
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Tech Stack
+      </motion.h2>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4"
+      >
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaHtml5 className="text-7xl text-orange-400" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <IoLogoCss3 className="text-7xl text-blue-500" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(2)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <IoLogoJavascript className="text-7xl text-yellow-300" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <RiReactjsLine className="text-7xl text-cyan-400" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaNodeJs className="text-7xl text-green-500" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <BiLogoPostgresql className="text-7xl text-sky-700" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaAws className="text-7xl text-white-700" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(6)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaPython className="text-7xl text-yellow-300" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaDocker className="text-7xl text-blue-700" />
+        </motion.div>
+      </motion.div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Technologies
+export default Technologies;
