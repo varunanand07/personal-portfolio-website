3373bd1653f77a292dd22c8298f278c135f8fdcd
varunanand07 <anandvarun2003@gmail.com>
2024-10-05 20:16:43 +0100

    Added Framer Motion Animation to the Experiences section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,33 +3,53 @@ import { EXPERIENCES } from '../constants';
 import { FaGithub } from "react-icons/fa";
 import { HiDocumentText } from "react-icons/hi2";
 import { IoMdVideocam } from "react-icons/io";
+import { motion } from "framer-motion";
 
 const Experience = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-      <h2 className='my-20 text-center text-4xl'>Experience</h2>
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Experience
+      </motion.h2>
       <div>
         {EXPERIENCES.map((experience, index) => (
-          <div key={index} className='mb-8 flex flex-wrap lg:justify-center'>
-            <div className="w-full lg:w-1/4">
-              <p className='mb-2 text-sm text-neutral-400'>{experience.year}</p>
-            </div>
-            <div className="w-full max-w-xl lg:w-3/4">
-              <h6 className='mb-2 font-semibold'>
-                {experience.role} - {" "}
-                <span className='text-sm text-purple-200'>
+          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 0.5 }}
+              className="w-full lg:w-1/4"
+            >
+              <p className="mb-2 text-sm text-neutral-400">{experience.year}</p>
+            </motion.div>
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: 100 }}
+              transition={{ duration: 0.5 }}
+              className="w-full max-w-xl lg:w-3/4"
+            >
+              <h6 className="mb-2 font-semibold">
+                {experience.role} -{" "}
+                <span className="text-sm text-purple-200">
                   {experience.company}
                 </span>
               </h6>
               <div className='mb-4 text-neutral-400'>
                 {experience.description.map((item, idx) => (
-                  <p key={idx} className="mb-4">{item}</p> // Add margin-bottom for spacing
+                  <p key={idx} className="mb-4">{item}</p>
                 ))}
               </div>
               {experience.technologies.map((tech, index) => (
-                <span key={index} className='mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>{tech}</span>
+                <span key={index} className='mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>
+                  {tech}
+                </span>
               ))}
-            </div>
+            </motion.div>
             <div className="rounded-2xl p-4 flex flex-col items-center justify-center" style={{ width: '100px', height: '100%' }}>
               {experience.showGithub && (
                 <a href={experience.githubLink} target="_blank" rel="noopener noreferrer">
@@ -41,7 +61,7 @@ const Experience = () => {
                   <HiDocumentText className="text-5xl text-white-300 mt-10" />
                 </a>
               )}
-              {experience.videoLink && ( // Add video icon if video link exists
+              {experience.videoLink && (
                 <a href={experience.videoLink} target="_blank" rel="noopener noreferrer" className="mt-2">
                   <IoMdVideocam className="text-5xl text-blue-600 mt-10" />
                 </a>
