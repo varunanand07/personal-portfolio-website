b92df5ca81d01ade2bccb3b76cfc5aa62f45a4cc
varunanand07 <anandvarun2003@gmail.com>
2024-10-05 17:48:34 +0100

    Added functionality that displays the GitHub logo next to the projects and experiences with contains a link that can be clicked on which wwould take the user to the relevent github page

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,34 +1,39 @@
-import React from 'react'
-import { EXPERIENCES } from '../constants'
+import React from 'react';
+import { EXPERIENCES } from '../constants';
+import { FaGithub } from "react-icons/fa";
 
 const Experience = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-        <h2 className='my-20 text-center text-4xl'>Experience</h2>
-        <div>
-            {EXPERIENCES.map((Experience,index) => (
-                <div key={index} className='mb-8 flex flex-wrap lg:justify-center'
-                >
-                    <div className="w-full lg:w-1/4">
-                        <p className='mb-2 text-sm text-neutral-400'>{Experience.year}</p>
-                    </div>
-                    <div className="w-full max-w-xl lg:w-3/4">
-                      <h6 className='mb-2 font-semibold'>
-                        {Experience.role} - {" "}
-                        <span className='text-sm text-purple-200'>
-                          {Experience.company}
-                        </span>
-                      </h6>
-                      <p className='mb-4 text-neutral-400'>{Experience.description}</p>
-                      {Experience.technologies.map((tech, index) => (
-                        <span key={index} className='mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>{tech}</span>
-                      ))}
-                    </div>
-                </div>
-               ))}
-        </div>
+      <h2 className='my-20 text-center text-4xl'>Experience</h2>
+      <div>
+        {EXPERIENCES.map((experience, index) => (
+          <div key={index} className='mb-8 flex flex-wrap lg:justify-center'>
+            <div className="w-full lg:w-1/4">
+              <p className='mb-2 text-sm text-neutral-400'>{experience.year}</p>
+            </div>
+            <div className="w-full max-w-xl lg:w-3/4">
+              <h6 className='mb-2 font-semibold'>
+                {experience.role} - {" "}
+                <span className='text-sm text-purple-200'>
+                  {experience.company}
+                </span>
+              </h6>
+              <p className='mb-4 text-neutral-400'>{experience.description}</p>
+              {experience.technologies.map((tech, index) => (
+                <span key={index} className='mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>{tech}</span>
+              ))}
+            </div>
+            <div className="rounded-2xl p-4">
+              <a href={experience.githubLink} target="_blank" rel="noopener noreferrer">
+                <FaGithub className="text-7xl text-purple-800" />
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Experience
+export default Experience;
--- src/constants/index.js
+++ src/constants/index.js
@@ -7,14 +7,19 @@ export const HERO_CONTENT = `I'm a 21-year-old Software Developer based in Dubli
 I'm currently in the process of finishing my degree in Computer Science from Trinity College Dublin.
 I'm currently working with my professor on an Automatic Video Translation Application using Natural Language Processing. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality business solutions. Outside of coding, I enjoy staying active, playing the guist, exploring new technologies, and contributing to open-source projects. Check out my C.V or connect with me on LinkedIn, I'm always happy to meet new people! `;
 
-export const ABOUT_TEXT = `I am Beyond my individual skills, I understand that our field is built on team work, something I pride myself on. I
+export const ABOUT_TEXT = `With me, you get a straight-forward, hardworking individual who thrives in the world of Software
+Development. I love rising to the occasion, and often do my best work in situations alien to me. My resume
+speaks for itself – I’ve put in the hours, learnt from the best, and left every boss I’ve had thrilled with my
+contributions. I adapt to new environments rapidly – whether it’s in Dell Technologies, a company ranked
+31st on the list of Fortune 500 companies or if it is Liferay International, a relatively smaller, open source
+company that provides enterprise solutions, I have a proven track record of translating ideas to code, building
+full scale applications, working with professional teams, and producing workable business solutions. Beyond my individual skills, I understand that our field is built on team work, something I pride myself on. I
 am ready to take the lead if given a shot, and I know when take a backseat and provide support to my
 colleagues and superiors. I am an effective communicator with all those I come across – technical
 background or not. At the same time, I am constantly looking to learn from my fellow software engineers
 and supervisors; every mistake is a new chance to do it right, and every suggestion a new tool in my arsenal.
 Team work aside, I recognise the need for individual contributions. From building side projects to working
-after hours to learn new skills, this is not something I am unfamiliar with. Even now, I spend hours trying to learn more about cutting-edge
-work being done in data science, so I can present new resources to any team I work with.`
+after hours to learn new skills, this is not something I am unfamiliar with. `
 export const EXPERIENCES = [
   {
     year: "Jan 2024 - May 2024",
@@ -25,6 +30,7 @@ export const EXPERIENCES = [
     Mentored a team of five second-year students in best web development practices within a collaborative and Agile
     Framework, fostering a proactive learning environment.`,
     technologies: ["Javascript", "HTML", "CSS", "SpringMVC", "LiferayDXP"],
+    githubLink: "",
   },
   {
     year: "Jan 2023 - May 2023",
@@ -33,6 +39,7 @@ export const EXPERIENCES = [
     description: `Engineered a Machine Learning Named Entity Recognition (NER) Model tuned to DELL data to recognize important entities within customer support textual logs such as person names, addresses, technical and non-technical issues. Leveraged open-source NER models (SpaCy, Flair), advanced NLP techniques including Deep Learning and trained the model on proprietary DELL data to recognize key entities specific to DELL's customer service environment. Reduced manual entity identification time by 33%, increasing the efficiency of DELL customer support. Analyzed 500+ Software Requirements monthly and collaborated with other engineers within an Agile Development
     Environment to manage project workflow.`,
     technologies: ["Natural Language Processing", "Python", "spaCy", "AllenNLP", "ChatGPT API"],
+    githubLink: "https://github.com/varunanand07/SwEng-Group-1-NER",
   },
 ];
 
@@ -45,6 +52,7 @@ export const PROJECTS = [
       accurate real-time updates. Orchestrated Dockerized Postgres for efficient data management, simplifying the deployment process for database-related
       tasks.`,
     technologies: ["Java", "Spring Boot", "Dockerized Postgres", "JWT Authentication"],
+    githubLink: "https://github.com/varunanand07/bus-notifier-app",
   },
   {
     title: "Portfolio Website",
@@ -52,14 +60,16 @@ export const PROJECTS = [
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
     technologies: ["HTML", "CSS", "React", "Tailwind CSS"],
+    githubLink: "https://github.com/varunanand07/personal-portfolio-website",
   },
   {
     title: "Morse Code Game",
     image: project2,
     description:
-      `eveloped a Game aimed at teaching Morse Code, implementing two difficulty levels and utilizing Raspberry Pi Pico's RGB LED for feedback. Collaborated in an Agile Team, ensuring robust and timely project deliverables that aligned with industry standards. Developed comprehensive code components, including C functions, ARM Assembly, and Morse code buffer logic,
+      `Developed a Game aimed at teaching Morse Code, implementing two difficulty levels and utilizing Raspberry Pi Pico's RGB LED for feedback. Collaborated in an Agile Team, ensuring robust and timely project deliverables that aligned with industry standards. Developed comprehensive code components, including C functions, ARM Assembly, and Morse code buffer logic,
       contributing to the successful creation of an engaging educational game.`,
     technologies: ["C", "Arm Assembly"],
+    githubLink: "https://github.com/varunanand07/morse-code-game/tree/main/assign02",
   },
 ];
 
